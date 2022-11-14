19db2aa68c966bd5a7a63737c9adabde42b84072
dandyfadiah08 <72121652+dandyfadiah08@users.noreply.github.com>
2022-11-14 08:49:46 +0700

    4 files modified

--- src/header.js
+++ src/header.js
@@ -3,20 +3,13 @@ import "./style.css";
 
 export default function App() {
   return (
+    
     <div id="header">
-      <div class="row">
-        <div class="col-md-6">
-          <a id="logo">Social</a>
-          <a id="logo2">Network</a>
-          
-        </div>
-        <div class="col-md-6">
-        <div class="input-group">
-  <input type="search" class="form-control rounded" placeholder="find" aria-label="Search" aria-describedby="search-addon" />
-  <button type="button" class="btn btn-outline-primary">Find</button>
-</div>
-        </div>
-      </div>
+      <a id="logo">Social</a>
+      <a id="logo2">Network</a>
+      <input placeholder="Find" class="form-group" type="text"/>
+      <button class="form-group2">Upload</button>
+      <span class="form-group3"></span>
     </div>
   );
 }
