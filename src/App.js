d5a182f060c2cfefd568c8ac2dd44c32fc1b1255
dandyfadiah08 <72121652+dandyfadiah08@users.noreply.github.com>
2022-11-14 09:15:31 +0700

    2 files modified

--- src/App.js
+++ src/App.js
@@ -4,8 +4,13 @@ import "./style.css";
 export default function App() {
   return (
     <div id="menu">
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen:)</p>
+      <nav aria-label="breadcrumb">
+  <ol class="breadcrumb">
+    <li class="breadcrumb-item"><a href="#">Home</a></li>
+    <li class="breadcrumb-item"><a href="#">Library</a></li>
+    <li class="breadcrumb-item active" aria-current="page">Data</li>
+  </ol>
+</nav>
     </div>
   );
 }
