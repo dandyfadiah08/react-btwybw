895cce8684ddfcefb1f34d00a97903019e583802
dandyfadiah08 <72121652+dandyfadiah08@users.noreply.github.com>
2022-11-15 19:38:32 +0700

    3 files modified, 1 added

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import "./style.css";
 import App from './App';
 import Header from './header';
 import Section1 from './section1';
+import Section2 from './section2';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
--- src/section2.js
+++ src/section2.js
@@ -4,8 +4,7 @@ import "./style.css";
 export default function App() {
   return (
     
-    <div id="section1">
-      
+    <div id="section2">
       <table width = "100%" border = "1">
           
 
@@ -62,62 +61,7 @@ export default function App() {
 
            
        </table>
-       <table width = "100%" border = "1">
-          
-
-          <tr valign = "top">
-             <td bgcolor = "#cd5c5c" width = "400">
-              <tr>
-                <td width="200">
-                  <h3 id="logo" >Videos</h3>
-                </td>
-                <td width="200" align="right">
-                  
-                  <p id="logo6">Browser all Views</p>
-                </td>
-                <td width="200">
-                </td>
-              </tr>
-              <tr>
-                <td 
-                height="150"
-                colSpan="2"
-                rowSpan="2"
-                >
-                  <img width="300" height="330" src="https://yt3.ggpht.com/a/AATXAJxdFBJTAfTbhjGVpXHuVMbNcDqXRlzoaly1xg=s900-c-k-c0xffffffff-no-rj-mo" alt=""/>
-                  
-                </td>
-
-                <td>
-                <img width="150" height="150" src="https://yt3.ggpht.com/a/AATXAJxdFBJTAfTbhjGVpXHuVMbNcDqXRlzoaly1xg=s900-c-k-c0xffffffff-no-rj-mo" alt=""/>
-                </td>
-              </tr>
-              <tr>
-
-                <td>
-                <img width="150" height="150" src="https://yt3.ggpht.com/a/AATXAJxdFBJTAfTbhjGVpXHuVMbNcDqXRlzoaly1xg=s900-c-k-c0xffffffff-no-rj-mo" alt=""/>
-                </td>
-              </tr>
-              <tr>
-                <td>
-                <img width="150" height="150" src="https://yt3.ggpht.com/a/AATXAJxdFBJTAfTbhjGVpXHuVMbNcDqXRlzoaly1xg=s900-c-k-c0xffffffff-no-rj-mo" alt=""/>
-                </td>
-                <td>
-                <img width="150" height="150" src="https://yt3.ggpht.com/a/AATXAJxdFBJTAfTbhjGVpXHuVMbNcDqXRlzoaly1xg=s900-c-k-c0xffffffff-no-rj-mo" alt=""/>
-                </td>
-                <td>
-                <img width="150" height="150" src="https://yt3.ggpht.com/a/AATXAJxdFBJTAfTbhjGVpXHuVMbNcDqXRlzoaly1xg=s900-c-k-c0xffffffff-no-rj-mo" alt=""/>
-                </td>
-              </tr>
-             </td>
-              
-             <td bgcolor = "#eee" >
-                Teknik dan Pengaturan Tutorial
-             </td>
-          </tr>
-
-           
-       </table>
+       
     </div>
   );
 }
