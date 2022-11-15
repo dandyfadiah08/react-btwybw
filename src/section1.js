b739ff72b2749597b47eda84a2bec4c10edd2d8d
dandyfadiah08 <72121652+dandyfadiah08@users.noreply.github.com>
2022-11-15 19:07:30 +0700

    2 files modified

--- src/section1.js
+++ src/section1.js
@@ -5,11 +5,11 @@ export default function App() {
   return (
     
     <div id="section1">
-      <table width = "100%" border = "0">
+      <table width = "100%" border = "1">
           
 
           <tr valign = "top">
-             <td bgcolor = "#aaa" width = "400">
+             <td bgcolor = "#cd5c5c" width = "400">
               <tr>
                 <td width="200">
                   <h3 id="logo" >Videos</h3>
@@ -22,39 +22,40 @@ export default function App() {
                 </td>
               </tr>
               <tr>
-                <td bgcolor="white"
-                height="100"
-                border = "1"
+                <td 
+                height="150"
                 >
+                  <img width="150" height="150" src="https://yt3.ggpht.com/a/AATXAJxdFBJTAfTbhjGVpXHuVMbNcDqXRlzoaly1xg=s900-c-k-c0xffffffff-no-rj-mo" alt=""/>
+                  
                 </td>
-                <td bgcolor="white"
+                <td
                 height="100">
-                  hjhj
+                  <img width="150" height="150" src="https://yt3.ggpht.com/a/AATXAJxdFBJTAfTbhjGVpXHuVMbNcDqXRlzoaly1xg=s900-c-k-c0xffffffff-no-rj-mo" alt=""/>
                 </td>
                 <td>
-                  kjhkjh
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  ijoi
-                </td>
-                <td>
-                  hjhj
-                </td>
-                <td>
-                  kjhkjh
+                <img width="150" height="150" src="https://yt3.ggpht.com/a/AATXAJxdFBJTAfTbhjGVpXHuVMbNcDqXRlzoaly1xg=s900-c-k-c0xffffffff-no-rj-mo" alt=""/>
                 </td>
               </tr>
               <tr>
                 <td>
-                  ijoi
+                <img width="150" height="150" src="https://yt3.ggpht.com/a/AATXAJxdFBJTAfTbhjGVpXHuVMbNcDqXRlzoaly1xg=s900-c-k-c0xffffffff-no-rj-mo" alt=""/>
                 </td>
                 <td>
-                  hjhj
+                <img width="150" height="150" src="https://yt3.ggpht.com/a/AATXAJxdFBJTAfTbhjGVpXHuVMbNcDqXRlzoaly1xg=s900-c-k-c0xffffffff-no-rj-mo" alt=""/>
                 </td>
                 <td>
-                  kjhkjh
+                <img width="150" height="150" src="https://yt3.ggpht.com/a/AATXAJxdFBJTAfTbhjGVpXHuVMbNcDqXRlzoaly1xg=s900-c-k-c0xffffffff-no-rj-mo" alt=""/>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                <img width="150" height="150" src="https://yt3.ggpht.com/a/AATXAJxdFBJTAfTbhjGVpXHuVMbNcDqXRlzoaly1xg=s900-c-k-c0xffffffff-no-rj-mo" alt=""/>
+                </td>
+                <td>
+                <img width="150" height="150" src="https://yt3.ggpht.com/a/AATXAJxdFBJTAfTbhjGVpXHuVMbNcDqXRlzoaly1xg=s900-c-k-c0xffffffff-no-rj-mo" alt=""/>
+                </td>
+                <td>
+                <img width="150" height="150" src="https://yt3.ggpht.com/a/AATXAJxdFBJTAfTbhjGVpXHuVMbNcDqXRlzoaly1xg=s900-c-k-c0xffffffff-no-rj-mo" alt=""/>
                 </td>
               </tr>
              </td>
