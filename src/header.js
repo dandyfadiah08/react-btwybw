b4db0e6b8e9a68f257ecde61dfde69c7bcf71114
dandyfadiah08 <72121652+dandyfadiah08@users.noreply.github.com>
2022-11-16 10:17:21 +0700

    2 files modified

--- src/header.js
+++ src/header.js
@@ -7,9 +7,13 @@ export default function App() {
     <div id="header">
       <a id="logo">Social</a>
       <a id="logo2">Network</a>
-      <input placeholder="Find" class="form-group" type="text"/>
-      <button class="form-group2">Upload</button>
-      <span class="form-group3"></span>
+      <input placeholder="Find" class="form-group" type="text"/><i class="fa fa-search lop"></i>
+      <button class="form-group2">&nbsp;<i class="fa fa-arrow-up" aria-hidden="true"></i> Upload</button>
+    <a href="">
+      <button class="form-group3"><li class="fa fa-user user"></li></button>
+      <p class="form-group5"><b> Wesheem </b></p>
+      <p class="form-group6">Arshad</p>
+      </a>
     </div>
   );
 }
